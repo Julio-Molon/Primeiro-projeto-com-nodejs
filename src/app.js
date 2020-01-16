035444bd9936bdcbe1e5a3f7a5110f362d5d5721
Júlio <juliomolon@live.com>
2020-01-16 16:24:10 -0300

    Inicializando os modelos no sequelize

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import routes from './routes';
+import './database';
 // const middlewares = require("./middlewares");
 
 class App {
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,17 @@
-const { Router } = require('express');
+import { Router } from 'express';
+import User from './app/models/user';
 
 const routes = new Router();
+
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Julio César',
+    email: 'juliomolon@live.com',
+    password_hash: '123564541',
+  });
+
+  return res.json(user);
+});
 
 routes.get('/', (req, res) => {
   return res.json({ message: 'Hello World' });
