5efeaa86853cca69535724759db9191291fe7944
Júlio <juliomolon@live.com>
2020-01-25 01:05:09 -0300

    Criando fk da tabela de imagens do usuario, e rota para listar prestadores

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,9 @@
 import Sequelize from 'sequelize';
 import User from '../app/models/user';
+import Files from '../app/models/Files';
 import databaseConfig from '../config/database';
 
-const models = [User];
+const models = [User, Files];
 
 class Database {
   constructor() {
@@ -11,7 +12,10 @@ class Database {
 
   init() {
     this.connection = new Sequelize(databaseConfig);
-    models.map(model => model.init(this.connection));
+
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 }
 
