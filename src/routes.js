ef030af1c587affab6f610a3c47e021f9d972401
root <juliomolon@live.com>
2020-01-26 12:52:34 -0300

    Listagem de agendamentos do prestador

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ import ProviderController from './app/controllers/ProviderController';
 
 import authMiddleware from './app/middlewares/auth';
 import AppointmentController from './app/controllers/AppointmentController';
-import Appointment from './app/models/Appointment';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -24,6 +24,8 @@ routes.get('/providers', ProviderController.index);
 
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
