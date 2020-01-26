e585201e99dc94a78a29b3d1b6f32c228170b880
root <juliomolon@live.com>
2020-01-26 12:22:18 -0300

    Validacoes, listagem de agendamentos e paginacao

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import ProviderController from './app/controllers/ProviderController';
 
 import authMiddleware from './app/middlewares/auth';
 import AppointmentController from './app/controllers/AppointmentController';
+import Appointment from './app/models/Appointment';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -22,6 +23,7 @@ routes.get('/providers', ProviderController.index);
 //
 
 routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
